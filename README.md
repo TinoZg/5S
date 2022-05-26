9b0279f1aa95dd5d8e248d53ecb7f5a0dece45e9
tinozg <valentino.novakk@gmail.com>
2022-05-26 13:33:56 +0200

    Dodani praznici za 2022

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 5S raspored za kuhinju
+
+[Link](https://tinozg.github.io/5S/index.html) za stranicu
--- utilities.js
+++ utilities.js
@@ -88,4 +88,17 @@ export function dodajDane(datum, dana) {
 
 export const praznici = {
   "2022-01-01": "Nova Godina",
+  "2022-01-06": "Sveta tri kralja",
+  "2022-04-17": "Uskrs",
+  "2022-04-18": "Uskrsni ponedjeljak",
+  "2022-05-01": "Praznik rada",
+  "2022-05-30": "Dan državnosti",
+  "2022-06-16": "Tijelovo",
+  "2022-06-22": "Dan antifašističke borbe",
+  "2022-08-05": "Dan domovinske zahvalnosti",
+  "2022-08-15": "Velika Gospa",
+  "2022-11-01": "Dan svih svetih",
+  "2022-11-18": "Sjećanje na Vukovar",
+  "2022-12-25": "Božić",
+  "2022-12-26": "Sveti Stjepan",
 };
